9f242a24f57402e6902fe1ae4a6aa1f7ea204ec8
Pedro Bruno <55460469+pedro-bX1@users.noreply.github.com>
2021-02-24 01:03:37 -0300

    Add files via upload

---
+++ loginsistema5/professor.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área do professor</title>
+</head>
+<body>
+    professor
+</body>
+</html>
