b93ff9b6e420f85d391773af5d1201b40e571164
Pedro Bruno <55460469+pedro-bX1@users.noreply.github.com>
2021-06-30 23:26:14 -0300

    modificações

---
+++ mpdfmetodopost/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="makepdf.php">
+    <input type="text" name="fname" placeholder="primeiroNome">
+    <input type="text" name="Iname" placeholder="segundoNome">
+    <input type="email" name="email" placeholder="Email">
+    <textarea name = "messagem" placeholder="Sua mensagem"></textarea>
+    <button type="submit"> CREATE PDF</button>
+    </form>
+</body>
+</html>
